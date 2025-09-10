b2eec91ba2b428f3e7973a535a1ca8b5d86ec96b
stupid-automations <66000989+stupid-automations@users.noreply.github.com>
2025-09-10 23:44:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Get the directory where app.py is located
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Set the template folder dynamically
+template_path = os.path.join(base_dir, "templates")
+
+app = Flask(__name__, template_folder=template_path)
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # Render HTML page
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
